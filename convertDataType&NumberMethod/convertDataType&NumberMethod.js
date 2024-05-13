2c4efb2e88bd2ec6e4e218fba34550aef9a396a3
Imam Hossain <supermanbd1996@gmail.com>
2024-05-14 04:25:25 +0600

    Java-scriptPractice

---
+++ convertDataType&NumberMethod/convertDataType&NumberMethod.js
@@ -0,0 +1,57 @@
+// var num = 12;
+
+// num = toString(num); //to convert from no to string
+
+// console.log(typeof(num)); // to check output of data type
+
+
+
+
+// var num = "113";
+
+// num = parseInt(num); //to convert string to Integer
+
+// console.log(typeof(num));
+
+
+
+
+// var no = "12.4"; //this is a float number in string data type
+
+// no = parseFloat(no); //convert to float no. in js integer and float is count for number
+
+// console.log(typeof(no));
+
+
+
+// var num = 5.5678;
+
+// console.log(num.toFixed(2)); //that means how many no we would like to show after decimal
+
+
+
+// var num = 5.5678;
+
+// console.log(num.toPrecision(1)); // how many number we want to show of total number
+
+
+
+// console.log(Number("12")); // by using this Number(); function we can convert any data type to number
+
+
+// if we would like to check data type 
+console.log(typeof(Number("12")));
+
+
+
+console.log((Number(true))); //convert from boolean to number
+
+
+console.log(Number(false)); //convert from boolean to number
+
+console.log(typeof(Number(true))); //convert from boolean to number and show data type
+
+
+
+
+
