a6769468e4a5ad014362ee71acf3c342ab6721b3
Imam Hossain <supermanbd1996@gmail.com>
2024-05-27 01:16:35 +0600

    new practice add

---
+++ Array/Array-with-Loop/index.js
@@ -0,0 +1,54 @@
+//user er kas theke 4ti no nibo and arra and loop er sahajje sei 5ti no print korbo and 5ti no er jog fol print korbo
+// var num = new Array();
+// for(var i = 0; i < 5; i++){
+//     num[i] = parseInt(prompt("Enter a number: "));
+// } 
+// var sum = 0;
+// for(i=0; i<5; i++){
+//     console.log(num[i]);
+//     sum = sum + num[i];
+// }
+// console.log("sum = "+ sum);
+
+
+
+
+
+// var num = new Array();
+// var sum = 0; 
+// for(var x=0; x<=5; x++){
+//     num[x] = parseInt(prompt("Enter a number: "));
+//     console.log(num[x]);
+//     sum = sum + num[x];
+// }
+
+// console.log("Sum = ".concat(sum));
+
+
+var number = [20, 21, 22, 25];
+var sum = 0;
+for(var x=0; x<4; x++){
+    console.log(number[x]);
+    sum = sum + number[x];
+
+}
+
+console.log("Result = ".concat(sum));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Array/index.js
@@ -0,0 +1,4 @@
+var names = ["Imam", "Mariya", "Mim"];
+        //remove last value of the array
+        names.pop();
+        console.log(names);
